13960b8fa69c623139a9aa04a78d4e7996510e27
nehuencabibbo <ncabibbo@fi.uba.ar>
2024-09-03 03:19:30 -0300

    Fix for automatic tests

---
+++ validar-echo-server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+docker build ./validar_echo_server -t validador_echo_server > /dev/null 2>&1
+
+config_file="./server/config.ini"
+
+server_tag=$(grep -i 'SERVER_IP' "$config_file" | awk -F'=' '{print $2}' | xargs)
+server_port=$(grep -i 'SERVER_PORT' "$config_file" | awk -F'=' '{print $2}' | xargs)
+
+message_sent="Hello echo server!"
+
+docker run -d --name validador --network tp0_testing_net validador_echo_server "$server_tag" "$server_port" "$message_sent" > /dev/null 2>&1
+
+response=$(docker logs validador)
+
+if [ "$response" == "$message_sent" ]; then
+  echo "action: test_echo_server | result: success"
+else 
+  echo "action: test_echo_server | result: fail."
+fi
+
+docker stop validador > /dev/null 2>&1 #Discard both stdout and err
+docker rm validador > /dev/null 2>&1
+docker rmi validador_echo_server > /dev/null 2>&1
